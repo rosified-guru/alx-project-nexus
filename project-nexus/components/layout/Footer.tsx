5d21f74d625f7f47ca2863ab03f1d12483f7ef37
Rose Mwangi <muthonirosee7@gmail.com>
2025-08-03 20:58:38 +0300

    Project Setup

--- project-nexus/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-200 text-center p-4 text-sm text-gray-700">
-      © 2025 Haven Cart. All rights reserved.
-    </footer>
-  );
-}
